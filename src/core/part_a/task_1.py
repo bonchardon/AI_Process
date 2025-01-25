6b10db0cec2721aac81a1367284f4c8516f344eb
mborodada <mboroda@lytvynov-production.com>
2025-01-25 16:03:35 +0200

    initial files

---
+++ src/core/part_a/task_1.py
@@ -0,0 +1,7 @@
+# todo: Design  comprehensive prompt th instrcts n AI gent to hnde tsks specific to yor
+#  chosen scenrio.
+
+class PromptTask:
+
+    def __init__(self):
+        ...
---
+++ src/core/part_b/task_2.py
@@ -0,0 +1,5 @@
+# todo: Visize the process fow between different gents incding
+
+class Vizualizationpart:
+    def __init__(self):
+        ...
